085bbb6bb90c93071b8731297ca999125f8fddb7
Yamil <yamilsaad00@gmail.com>
2025-08-04 18:26:00 -0300

    correccion de efectos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Menu from './pages/Menu'
@@ -84,7 +84,7 @@ function App() {
       {/* Contenido principal */}
       <main>
         {currentPage === 'inicio' && (
-          <InicioPage onNavigateToMenu={(business) => setCurrentPage('home')} />
+          <InicioPage onNavigateToMenu={() => setCurrentPage('inicio')} />
         )}
 
         {currentPage === 'home' && (
